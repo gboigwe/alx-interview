ac50ae0a0cee8974bc27a33f34a10545aa45c697
Gbolahan Akande <geakande@gmail.com>
2024-07-25 22:41:21 +0000

    Pascal Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def pascal_triangle(n):
     """
     Generate Pascal's triangle of n rows.
@@ -8,10 +9,6 @@ def pascal_triangle(n):
     Returns:
         list: A list of lists of integers representing Pascal's triangle.
         Returns an empty list if n <= 0.
-
-    Example:
-        >>> pascal_triangle(5)
-        [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
     """
     if n <= 0:
         return []
