e93c0174f81ddf2040f69f144ce5b98f35587faf
gboigwe <geakande@gmail.com>
2024-08-11 21:40:15 +0100

    Module that can perform minimum operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""This code contains function for minimum operations"""
+
+
+def minOperations(n):
+    """Performing operations needed to result in
+    n H character times
+        Return 0 if n cannot be obtained
+    """
+    if n <= 1:
+        return 0
+
+    min_ope = 0
+    div = 2
+    while n > 1:
+        while n % div == 0:
+            min_ope += div
+            n //= div
+        div += 1
+    return min_ope
